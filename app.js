fe867351973ea8ba49ba1a5039ee5952f9b5811f
uday <udaychandrag@gmail.com>
2021-12-03 16:57:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const express= require('express');
+
+const app= express();
+app.use(express.json())
+
+app.use('/add-product',(req,res,next)=>{
+   
+    res.send("<form action='/product' method='post'><input type='text'><button type='submit'>Add product</button></form>");
+})
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
+  
+   res.send("<h1>Hello from Express</h1>")
+})
+
+
+
+app.listen(3000);
