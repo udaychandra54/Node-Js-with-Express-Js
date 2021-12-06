95b84e954461116b074be714547a95e2a1c402e3
uday <udaychandrag@gmail.com>
2021-12-06 12:00:24 +0530

    added pug-engine dynamic content

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,16 +1,24 @@
+const path = require('path');
+
 const express = require('express');
-const path = require('path')
-const rootDir = require('../helpers/path')
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
-router.get('/add-product',(req,res,next)=>{
-   
-    res.sendFile(path.join(rootDir,'views','add-product.html'))
-})
+const products = [];
 
-router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product',{docTitle:'Add Product',path:'/admin/add-product'})
+});
 
-module.exports=router;
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
