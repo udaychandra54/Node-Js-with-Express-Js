3b403e4318da04906ef888bbe830a95935139941
uday <udaychandrag@gmail.com>
2021-12-06 16:22:53 +0530

    hbs engine template code added

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const path = require('path')
-const rootDir = require('../helpers/path')
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
-router.get('/add-product',(req,res,next)=>{
-   
-    res.sendFile(path.join(rootDir,'views','add-product.html'))
-})
+const products = [];
 
-router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render("add-product", {
+    docTitle: "Add Product",
+    path: "/admin/add-product",
+    layout: false,
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+});
 
-module.exports=router;
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+});
+
+exports.routes = router;
+exports.products = products;
--- Routes/shop.js
+++ Routes/shop.js
@@ -1,11 +1,23 @@
+const path = require('path');
+
 const express = require('express');
-const path= require('path');
-const rootDir = require('../helpers/path')
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
-router.get('/',(req,res,next)=>{
-  
-    res.sendFile(path.join(rootDir,'views','shop.html'))
- })
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+    layout:false
+  });
+});
 
-module.exports=router;
+module.exports = router;
